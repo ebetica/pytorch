1f971705312c0c75e903d58843187d9c2cfa658f
Zeming Lin <ebetica0@gmail.com>
2018-10-16 19:31:53 -0700

    Export generators

--- aten/src/ATen/templates/GeneratorDerived.h
+++ aten/src/ATen/templates/GeneratorDerived.h
@@ -9,7 +9,7 @@
 namespace at {
 
 class Context;
-struct ${name}Generator : public Generator {
+struct CAFFE2_API ${name}Generator : public Generator {
   ${name}Generator(Context * context);
   CAFFE2_API virtual ~${name}Generator();
 
